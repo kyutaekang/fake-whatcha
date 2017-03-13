5a2fdc442060d481c0be57792507a4cb7b5807a4
kyutae kang <kyutae.kang@traport.com>
2017-03-13 20:26:18 +0900

    no message

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-header',
@@ -7,17 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private angularFire: AngularFire) {
+  }
+
+  isProcessed: boolean = false;
+  isLogin: boolean = false;
 
   searchKeyword: string;
   userName: string;
   userProfilePicture: string;
 
   ngOnInit() {
+    this.angularFire.auth.subscribe(data => {
+      this.isProcessed = true;
+
+      if(data && data.uid) {
+        this.isLogin = true;
+        this.userName = data.auth.displayName;
+      }
+      else {
+        this.isLogin = false;
+        this.userName = undefined;
+      }
+    });
   }
 
   private _doSearch():void {
     console.log('_doSearch');
+  }
+
+  private doLogin(): void {
+    this.angularFire.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Redirect
+    });
+  }
+
+  private doLogout(): void {
+    this.angularFire.auth.logout();
   }
 
 }
